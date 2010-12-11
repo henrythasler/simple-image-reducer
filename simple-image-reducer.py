894ecc430b85ba10ec0fd71b092060374f55a413
lostclus <lostclus@187cd8ac-69df-42c5-93f5-9e0905c93663>
2010-12-11 12:01:13 +0000

    Changed web site to http://simple-image-reducer.org.ua.

--- simple-image-reducer.py
+++ simple-image-reducer.py
@@ -302,7 +302,7 @@ GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>."""))
-        dialog.set_website("http://www.lostclus.kiev.ua/")
+        dialog.set_website("http://simple-image-reducer.org.ua/")
         dialog.connect('response', lambda d, r: d.destroy())
         dialog.show()
 
